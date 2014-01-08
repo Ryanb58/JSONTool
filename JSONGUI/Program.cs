6f27ff3b16b802dae704fb91e483fd33eba0a0ef
Ryanb58 <ryanb58@live.com>
2014-01-07 19:59:05 -0500

    Added Minify.
    JSON now had the minify function, also added console and WPF apps. WPF
    will probably be more up to date.

---
+++ JSONGUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JSONTool;
+
+namespace JSONGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JSON jsonTool = new JSON();
+
+            Console.WriteLine("Please enter in your JSON below to minify");
+            string json = Console.ReadLine();
+            Console.WriteLine(jsonTool.minify(json));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JSONTool/JSON.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSONTool
+{
+    public class JSON
+    {
+        public string minify(string json)
+        {
+            //This will hold the result of the string.
+            string jsonResult = "";
+
+            //int to tell which " or ' you are at.
+            int quotes = 0;
+
+            //Loops through each character in the json sent.
+            foreach (char c in json)
+            {
+                
+                if (c.ToString() == "\"" || c.ToString() == "\'")
+                {
+                    quotes += 1;
+                    if (quotes == 2)
+                    {
+                        quotes = 0;
+                    }
+                }
+
+                //If any white or null spaces are found, remove them. Except in quotes.
+                if (!String.IsNullOrWhiteSpace(c.ToString()))
+                {
+                    jsonResult += c.ToString();
+                }
+                else if (quotes == 1)
+                {
+                    jsonResult += c.ToString();
+                }
+            }
+            //Return the json string.
+            return jsonResult;
+        }
+    }
+}
