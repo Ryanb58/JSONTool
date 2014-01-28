1e88db6d86471ca25810ab589ac1a41aeafac9e6
Travis Cunningham <travcunn@umail.iu.edu>
2014-01-27 21:47:57 -0500

    Update JSON.cs

--- JSONTool/JSON.cs
+++ JSONTool/JSON.cs
@@ -2,7 +2,7 @@ namespace JSONTool
 {
     public class JSON
     {
-        public static string minify(string json)
+        public string minify(string json)
         {
             //Return the json string.
             return System.Text.RegularExpressions.Regex.Replace(json, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
